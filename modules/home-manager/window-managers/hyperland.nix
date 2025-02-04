df9b6f8505c0d2f229027d3a2b9393bb72cc81a8
BashfulBandits <Devin-CH@protonmail.com>
2025-02-04 04:36:47 -0800

    added hyperland

---
+++ modules/home-manager/window-managers/hyperland.nix
@@ -0,0 +1,14 @@
+{ inputs, pkgs, ... }:
+
+{
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  environment.sessionVariables = {
+    enable = true;
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+  };
+}
