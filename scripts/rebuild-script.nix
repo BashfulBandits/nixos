91a3557ed5825ec3092cdccecd6eb80547382c41
BashfulBandits <Devin-CH@protonmail.com>
2025-02-15 14:51:40 -0800

    renamed rebuild script to be slightly more descrpitive

--- scripts/rebuild-script.nix
+++ scripts/rebuild-script.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 
-pkgs.writeShellScriptBin "nix-rebuild-script" ''
-  cd $HOME/nixos
-  ${pkgs.git}/bin/git add --all
-  sudo nixos-rebuild switch --flake $HOME/nixos#$1
+pkgs.writeShellScriptBin "nixos-rebuild-script" ''
+  cd $HOME/nixos &&
+  sudo nixos-rebuild switch --flake $HOME/nixos#$1 &&
 ''
+#${pkgs.git}/bin/git add --all 
 
