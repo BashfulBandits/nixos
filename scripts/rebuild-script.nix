77d9e4bfb878ed12e99269ddc06779739405d411
BashfulBandits <Devin-CH@protonmail.com>
2025-02-15 14:52:56 -0800

    renamed rebuild script to be slightly more descrpitive

--- scripts/rebuild-script.nix
+++ scripts/rebuild-script.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 
 pkgs.writeShellScriptBin "nixos-rebuild-script" ''
-  cd $HOME/nixos &&
-  sudo nixos-rebuild switch --flake $HOME/nixos#$1 &&
+  cd $HOME/nixos 
+  sudo nixos-rebuild switch --flake $HOME/nixos#$1 
 ''
