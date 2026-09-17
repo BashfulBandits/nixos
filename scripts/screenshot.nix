fa65f6d9eb791a3453a00e50a6e8e22eddabd5f6
Ontos <devin-ch@protonmail.com>
2026-09-17 02:03:12 -0700

    178         2026-09-17 02:03:09  26.11.20260616.567a49d  7.1.0   Unknown                 []              True

--- scripts/screenshot.nix
+++ scripts/screenshot.nix
@@ -1,16 +1,15 @@
-
 { pkgs, ... }:
 
 pkgs.writeShellScriptBin "screenshot" ''
   path=~/Pictures/Screenshots
   time=$(date +%Y-%m-%d-%H%M%S-%N)
   if [ -z "$1" ]; then
-      grim -c ~/Pictures/Screenshots/$time.png
+      grim -c $path/$time.png
       notify-send "Screenshot Saved: $time.png"
-  elif [ "$1" == "-S" || "$1" == "-selection" ]; then
+  elif [[ "$1" == "-S" || "$1" == "--selection" ]]; then
       grim -g "$(slurp)" -c $path/$time.png
       notify-send "Screenshot Saved: $time.png"
-  elif [ "$1" == "-M" || "$1" == "-monitor"]; then
+  elif [[ "$1" == "-M" || "$1" == "--monitor" ]]; then
        grim -o "$(hyprctl monitors -j | ${pkgs.jq}/bin/jq -r '.[] | select(.focused==true) | .name')" -c $path/$time.png
       notify-send "Screenshot Saved: $time.png"
   fi
